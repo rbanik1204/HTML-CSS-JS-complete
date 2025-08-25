58e9b1a11a3bacf9fc901436cc1c3119df6b87bd
rb1204 <ratulbanik2004@gmail.com>
2025-08-25 21:37:32 +0530

    Practice folder HTML_CSS_JS_PRACTICE modified

---
+++ HTML_CSS_JS_PRACTICE/HTML_CSS_JS_8/script.js
@@ -0,0 +1,6 @@
+let obj = document.body.children[1].firstElementChild;
+// const ChangeBgColor = () =>{
+//     obj.style.background = "red";
+// }
+// console.log("First child of body element node is",obj.firstChild);
+// console.log("First element node child of body Element node is",obj.firstElementChild);
