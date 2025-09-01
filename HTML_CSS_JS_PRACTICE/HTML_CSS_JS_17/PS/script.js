43b706e38c43ac81eeca3c2dd31e869f1205505b
rb1204 <ratulbanik2004@gmail.com>
2025-09-01 22:05:11 +0530

    PS of EventListener

---
+++ HTML_CSS_JS_PRACTICE/HTML_CSS_JS_17/PS/script.js
@@ -0,0 +1,13 @@
+let form = document.getElementById("forms");
+console.log(form.elements);
+let x = (e) =>{
+    alert(`This alert is popped by Submit`);
+    console.log(e.type);
+}
+let y = (e) =>{
+    alert(`This alert is popped by Reset`);
+    console.log(e.type);
+}
+form.password.autocomplete = 'on';
+form.Reset.addEventListener('click',y);
+form.Reset.addEventListener('click',x);
