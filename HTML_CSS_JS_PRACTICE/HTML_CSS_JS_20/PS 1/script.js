db2c25fd7ac338784e364dad311b801514f847cf
Ratul Banik <152281476+rbanik1204@users.noreply.github.com>
2025-09-06 02:56:57 +0530

    Corrected callBack Promises

--- HTML_CSS_JS_PRACTICE/HTML_CSS_JS_20/PS 1/script.js
+++ HTML_CSS_JS_PRACTICE/HTML_CSS_JS_20/PS 1/script.js
@@ -1,28 +1,29 @@
-function loadScript(src,myPromise = new Promise(function(myResolve){
+function loadScript(src,callBack){
+    console.log("Let's see when this works\n");
+    return myPromise = new Promise(function(myResolve){
     let script = document.createElement("script");
     script.src = src;
     console.log(myPromise);
     script.onload = (e)=>{
-        alert(src," has Successfully loaded");
+        console.log(src," has Successfully loaded");
         myResolve(true);
         console.log(myPromise);
         callBack(null,script);
     }
     script.onerror = (e)=>{
-        console.log(myPromise);
-        callBack(new Error("Faced Some error during Execution (target:",e.target,")"),script);
+        // console.log(myPromise);
+        myReject(new Error("Faced Some error during Execution"),script);
     }
-}),callBack){
-    console.log("Let's see when this works");
+});
 }
 function checkPromise(error,script){
     console.log("Hello from checkPromise function");
     if(error){
-        console(error.type,":",error.message);
+        console.log(error.type,":",error.message);
         console.log(script.src," can't be fetched!");
         return;
     }
     alert(myPromise," Executed Successfully");
 }
-let loaded = loadScript("script2.js",mypromise,checkPromise);
-console.log("caller sees:",loaded);
+let loaded = loadScript("script2.js",checkPromise);
+console.log("caller sees:",loaded.script.src);
