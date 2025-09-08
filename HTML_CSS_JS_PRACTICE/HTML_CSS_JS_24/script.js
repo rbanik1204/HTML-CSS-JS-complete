c40a0b47126488a4d03e6f3bbe61a0a3fbe5726f
Ratul Banik <152281476+rbanik1204@users.noreply.github.com>
2025-09-08 11:14:10 +0530

    try and catch learnt

---
+++ HTML_CSS_JS_PRACTICE/HTML_CSS_JS_24/script.js
@@ -0,0 +1,10 @@
+try{
+setTimeout(()=>{console.log("Hacking Wifi... please wait...");},1000);
+setTimeout(()=>{console.log("Fetching username & password please wait...");},2000);
+console.log(Ratul);
+setTimeout(()=>{console.log("Hacking Ratuls's facebook password... please wait...");},3000);
+setTimeout(()=>{console.log("Username and Password of Ratul's(+0123456789)... please wait...");},3000);
+}
+catch(error){
+    console.log(error.message);
+}
