35f90bb8e3f1d49bea9e22fdee061dd87e27b5f1
rb1204 <ratulbanik2004@gmail.com>
2025-10-16 19:06:31 +0530

    Practiced strings

---
+++ HTML_CSS_JS_PRACTICE/HTML_CSS_JS_REVISIT/index1.js
@@ -0,0 +1,9 @@
+let nam = 'Ratul Banik';
+console.log(nam.toLowerCase());
+console.log(nam.toUpperCase());
+console.log(nam.slice(2,5)); //slice(initial index,final index) but it doesnt include the last index element in sliced string
+let friend = 'Aditya'
+console.log(friend.concat(nam));
+let str = new String("Hello world");
+let val = str.valueOf();
+console.log(val);
