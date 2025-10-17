ea39ef7a6ae6d8708dbebaee062b32175874ff58
rb1204 <ratulbanik2004@gmail.com>
2025-10-17 23:54:51 +0530

    XMLHttpRequest object revisited

---
+++ HTML_CSS_JS_PRACTICE/HTML_CSS_JS_REVISIT/PS5/script.js
@@ -0,0 +1,20 @@
+let elem = document.getElementsByClassName('container')[0]
+function loadDoc(){
+    const xhr = new XMLHttpRequest();
+    xhr.open('GET','button.txt?user=Ratul',true);
+    xhr.setRequestHeader('Content-Type','text/html; charset=utf-8')
+    xhr.onreadystatechange = function (){
+        console.log('Current readyState:'+this.readyState);
+    }
+    xhr.onload = function() {
+        console.log('HTTP Response status:'+this.status)
+        responseDoc(xhr);
+    }
+    xhr.send();
+}
+elem.addEventListener('click',loadDoc)
+function responseDoc(xhttp){
+    console.log(xhttp.responseText)
+    elem.innerHTML = `<pre>${xhttp.responseText}</pre>`
+    console.log(xhttp.getResponseHeader("content-type"));
+}
