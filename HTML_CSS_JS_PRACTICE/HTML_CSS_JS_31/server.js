3bd74edf750bfbd0c15932d052b798732d2340df
rb1204 <ratulbanik2004@gmail.com>
2025-10-24 11:25:06 +0530

    query parameters

--- HTML_CSS_JS_PRACTICE/HTML_CSS_JS_31/server.js
+++ HTML_CSS_JS_PRACTICE/HTML_CSS_JS_31/server.js
@@ -1,19 +1,30 @@
 const http = require('http');
-
+const url = require('url');
+const fs = require('fs');
 //Creating new server instance
 const server = http.createServer((req,res)=>{
-    console.log("New request received!");
+    if (req.url === '/favicon.ico'){
+        return res.end();
+    }
+    console.log("New request received from "+req.url);
+    const Url = req.url;
+    const myUrl = url.parse(Url,true);
+    console.log("Query params"+myUrl.query);
+    console.log(myUrl);
+    var log = `${Date.now()}: New Request from ${Url}\n`
     //Multi route
-    switch(req.url){
+    fs.appendFile("log.txt",log,(err,data)=>{
+    switch(myUrl.pathname){
         case '/':
             res.end("Home page");
             break;
         case '/contact':
             res.end("Contact us");
             break;
-        // default:
-        //     res.end("404 response not send from server");
+        default:
+            res.end("404 response not send from server");
     }
+    })
 })
 const PORT = 8000
 server.listen(PORT,'127.0.0.1',()=>{
