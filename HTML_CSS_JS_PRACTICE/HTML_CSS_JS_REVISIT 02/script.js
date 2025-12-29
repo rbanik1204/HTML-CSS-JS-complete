746fc8d9147c2298f8c439fab1970eae7e7a9885
rB <ratulbanik2004@gmail.com>
2025-12-29 19:35:50 +0530

    Arrow Functions Revisit 02

---
+++ HTML_CSS_JS_PRACTICE/HTML_CSS_JS_REVISIT 02/script.js
@@ -0,0 +1,31 @@
+// //This is script.js
+// console.log("I'm back brother!");
+// // var a = 10;
+// // console.log(a);
+// // var a = 50;
+// // console.log(a);
+// const b =200;
+// console.log(b);
+// {
+//     const b = 100;
+//     console.log(b);
+// }
+
+// let a = 10;
+// let b = 10.5;
+// let c = "Hello World";
+// let d = true;
+// let e = null;
+// let f = undefined;
+// let g = Symbol("id");
+// console.log(typeof a,typeof b,typeof c,typeof d,typeof e,typeof f,typeof g);
+let array = [1,2,3,4,5];
+// console.log(Array.isArray(arr))
+// console.log(arr instanceof Array);
+
+
+//Function Invocation
+const sum=(arr)=>{
+    return arr.reduce((a,b)=>a+b)
+}
+console.log(sum(array))
