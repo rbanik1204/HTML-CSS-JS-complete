1d76a5409c76212ccc652b5ac3c460f6dbb31225
unknown <ratulbanik2004@gmail.com>
2026-01-11 01:20:25 +0530

    Array constructor & Prototype instances

---
+++ HTML_CSS_JS_PRACTICE/HTML_CSS_JS_REVISIT 02/Arrays/script1.js
@@ -0,0 +1,21 @@
+/* All contructor functions return an object by default */
+function Person(name,age){
+    this.name = name;
+    this.age = age;
+}
+const p1 = new Person("Ratul", 22);
+/* Here new creates an empty object {}
+ internally [[Prototype]] -> Person.prototype
+ Binds *this* to that object
+ Executes Person()
+ Returns the Object
+*/
+console.log(p1.name + p1.age)
+// The type casting of strings -> num =+"22" this is from String to Number Number.parseInt(str) or Number.parseFloat(str) 
+//from Number to String -> str = 22 + "" OR num.toString();
+console.log([1, 2, 3] instanceof Object)
+console.log(typeof Array(5)); //Initializes empty array of size 5
+//Array constructors methods 
+const arr = Array(5)
+arr+= [1,2,3]
+console.log(arr);
