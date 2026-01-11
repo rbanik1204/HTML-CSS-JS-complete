2b8c690be8a6e3c928859bed8bf882ec0713faf5
unknown <ratulbanik2004@gmail.com>
2026-01-12 01:38:50 +0530

    Completed Table Links

--- HTML_CSS_JS_PRACTICE/HTML_CSS_JS_REVISIT 02/table/script.js
+++ HTML_CSS_JS_PRACTICE/HTML_CSS_JS_REVISIT 02/table/script.js
@@ -1,1 +1,7 @@
 let t = document.querySelectorAll(".table")[0].children[0].firstElementChild
+console.log(t)
+let tBody = t.children[1]
+console.log(tBody.rows)
+console.log(t.children[0].rows[0].rowIndex);
+//SectionRowIndex -> Position of row within its section such as 
+//tHead,tBodies,tFoot
