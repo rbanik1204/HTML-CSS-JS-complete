d9bd1150857c16514c40071a5ad61171e19b239d
unknown <ratulbanik2004@gmail.com>
2026-02-01 21:28:30 +0530

    Prototype & Prototypal Inheritance

---
+++ HTML_CSS_JS_PRACTICE/HTML_CSS_JS_REVISIT 02/Prototype/script.js
@@ -0,0 +1,26 @@
+// const p1 = {
+//     lname : 'iwctl',
+//     fname : '204'
+// }
+// console.log(p1)
+// const p2 = Object.create(p1)
+// console.log(p2)
+// console.log(p2.fname===p2.__proto__.fname)
+// let fname = "iwctl"
+// //How fname has properties if it is a *string*
+// console.log(fname.__proto__) // references to String.prototype -> has the properties and methods of String 
+// console.log(String) //Base Class
+// console.log(String.prototype)
+// console.log(fname.__proto__ === String.prototype)
+//------------------------------Prototypal Inheritance-------------------------------
+let p1 = {
+    xp1: "Hello from p1",
+}
+let p2 = {
+    xp2: "Hello from p2",
+    __proto__ : p1,
+}
+let p3 = {
+    x3: "Hello from p3",
+    __proto__ : p2,
+}
