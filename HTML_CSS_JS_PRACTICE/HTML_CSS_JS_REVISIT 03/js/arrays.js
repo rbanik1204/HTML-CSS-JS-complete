af73d472b309095eda7a25bf295850f69529e74f
rbanik1204 <ratulbanik2004@gmail.com>
2026-08-22 17:05:10 +0530

    Prototypal & Constructor Array methods

---
+++ HTML_CSS_JS_PRACTICE/HTML_CSS_JS_REVISIT 03/js/arrays.js
@@ -0,0 +1,30 @@
+const promptSync = require('prompt-sync');
+const prompt = promptSync();
+let arr = prompt("Enter five Numbers separated by Commas: ")
+            
+arr = arr.split(',').map(Number)
+
+// Array.forEach((val,i) => {
+//     arr[i] = Number.parseInt(val)
+// });
+//Prototypal methods
+let a = arr.find(x=>x>3)
+let b = arr.findIndex(x=> x>=5)
+let c = arr.every(x=> x>=2)
+let d = arr.some(x=>x!==3)
+let e = arr.filter(x=> x<=2)
+console.log(arr)
+console.log(e)
+console.log(`a:${a} b:${b} c:${c} d:${d} e: ${e}`)
+arr.sort((a,b)=>a-b)//ascending order
+// arr = String(arr)//or arr.toString()
+console.log(arr)
+
+//Mutating prototypal methods
+/*
+push, pop, shift, unshift, splice, sort, reverse
+*/
+//Not Mutating
+/* 
+filter,reduce,slice,concat,flat,find,some,every
+*/
