43e89ab68819253ede9a26ccc4376168ebb08039
rbanik1204 <ratulbanik2004@gmail.com>
2026-08-22 13:35:22 +0530

    String Object ,Temporary Wrapper & String Literal

---
+++ HTML_CSS_JS_PRACTICE/HTML_CSS_JS_REVISIT 03/js/strings.js
@@ -0,0 +1,21 @@
+// let name="broke510"
+// console.log(name)
+// name[1]='c'//strings are immutable
+// console.log(name)
+let boy1 ='nikhil'
+let boy2 = 'pramod'
+console.log(`${boy1} is a friend of ${boy2}`)//template literals
+boy1 = boy1.toLocaleUpperCase()
+boy2 = boy1.concat(boy2," ",boy1.slice(1,4))
+console.log(boy2)
+// const str = new String("Hello");
+// console.log(str)
+//Other prototypal methods are
+/*
+str.charAt(Number)
+str.at(-1) //negative indexing possible
+str.startsWith("")
+str.endsWith("")
+str.indexOf("String")
+str.includes("String")
+*/
