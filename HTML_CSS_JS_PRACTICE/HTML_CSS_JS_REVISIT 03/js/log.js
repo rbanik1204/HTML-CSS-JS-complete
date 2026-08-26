b65d2ecb593d2a78af6851ffaf08ced86208e83b
rbanik1204 <ratulbanik2004@gmail.com>
2026-08-27 01:42:20 +0530

    .then and .catch success and error handlers in Promise

--- HTML_CSS_JS_PRACTICE/HTML_CSS_JS_REVISIT 03/js/log.js
+++ HTML_CSS_JS_PRACTICE/HTML_CSS_JS_REVISIT 03/js/log.js
@@ -1,4 +1,4 @@
-console.log("Reviting js!")
+console.log("Revisiting js!")
 const a = 10
 var b = 15
 console.log(a+b)
---
+++ HTML_CSS_JS_PRACTICE/HTML_CSS_JS_REVISIT 03/js/promise.js
@@ -0,0 +1,19 @@
+const loadScript = (src) => {
+    let Script = document.createElement("script");
+    Script.type = "text/javascript";
+    Script.src = src;
+    return new Promise((res, rej) => {
+        Script.onload = (event) => {
+            res(event.target);
+        };
+        Script.onerror = (event) => {
+            rej(new Error(`${event.target.src} did not download`))
+        };
+        document.head.appendChild(Script);
+    })
+}
+loadScript("log.js").then((value) => {
+    console.log(`${value.src} appended successfully!`)
+}, (err) => {
+    console.log("Error:"+err.message)
+})
