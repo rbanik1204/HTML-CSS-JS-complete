e699b6c56a697b5b2f3f4b5f040aa42053f8b24c
rbanik1204 <ratulbanik2004@gmail.com>
2026-08-28 17:32:33 +0530

    HTTP server authentication System

---
+++ HTML_CSS_JS_PRACTICE/HTML_CSS_JS_REVISIT 03/js/HTTP/Authentication/script.js
@@ -0,0 +1,27 @@
+const form = document.querySelector(".form");
+
+form.addEventListener("submit",(event)=>{
+    event.preventDefault();
+    console.log(event)
+    const username = document.querySelector("#username").value ;
+    const password = document.querySelector("#password").value;
+    const xhttp = new XMLHttpRequest();
+    xhttp.open("POST","http://[::1]:3000");
+    xhttp.onreadystatechange = (event)=>{
+        if(xhttp.readyState == 4 ){
+            if (event.target.status === 200) {
+                document.getElementById("message").innerHTML =
+                    event.target.responseText;
+                    event.target.style.color="white";
+                console.log(event.target.responseText)
+            } else {
+                document.getElementById("message").innerHTML =
+                    `HTTP error: ${event.target.status}`;
+            }
+        }
+    }
+    xhttp.setRequestHeader("Content-Type","application/json");
+    xhttp.send(JSON.stringify({username: username ,
+                                password: password
+    }));
+})
