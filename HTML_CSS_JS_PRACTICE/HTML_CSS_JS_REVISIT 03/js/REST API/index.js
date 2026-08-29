0b825a048f12f3772e16aeaeedf1fa3a2926acc5
rbanik1204 <ratulbanik2004@gmail.com>
2026-08-29 14:27:02 +0530

    REST API core design Principles

---
+++ HTML_CSS_JS_PRACTICE/HTML_CSS_JS_REVISIT 03/js/REST API/index.js
@@ -0,0 +1,55 @@
+const express = require('express')
+const fs = require('fs');
+const users = require("./MOCK_DATA.json")
+
+const app = express()
+const PORT = 3000
+
+//Register middleware
+app.use((req, res, next) => {
+    console.log(req.method, req.url)
+    next()
+})
+app.use(express.urlencoded({ extended: false }))
+
+app.get("/users", (req, res) => {
+    const html = `
+        <ul> 
+            ${users.map((user) => `<li>${user.first_name}</li>`).join("")}
+        </ul>
+    `
+    res.send(html);
+})
+
+app.post("/api/users", async (req, res) => {
+    //TODO: Create new User
+    users.push({ ...req.body, id: users.length + 1 })
+    fs.writeFile('./MOCK_DATA.json', JSON.stringify(users), (error, data) => {
+        return res.json({ status: "success", id:users.length })
+    })
+
+})
+//Routes
+app
+    .route("/api/users/:id")
+    .get((req, res) => {
+        const id = Number(req.params.id);
+        const user = users.find(user => user.id === id)
+        return res.send(user)
+    })
+    .post((req, res) => {
+        //TODO: Create user id 
+        return res.json({ status: "pending" })
+    })
+    .patch((req, res) => {
+        //TODO: Edit existing user using id
+        return res.json({ status: "pending" })
+    })
+    .delete((req, res) => {
+        //TODO: Delete user using id
+        return res.json({ status: "pending" })
+    })
+
+app.listen(PORT, "localhost", () => {
+    console.log(`Server running on PORT ${PORT}`);
+})
