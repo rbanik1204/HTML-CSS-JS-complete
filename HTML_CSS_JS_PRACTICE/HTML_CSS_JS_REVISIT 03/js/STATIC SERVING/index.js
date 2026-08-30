e93c8ac8d0ec11ba92ae6b03c59ed7679448c963
rbanik1204 <ratulbanik2004@gmail.com>
2026-08-30 18:27:39 +0530

    Middleware Serve Static Files: express.static

---
+++ HTML_CSS_JS_PRACTICE/HTML_CSS_JS_REVISIT 03/js/STATIC SERVING/index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+// app.use('/static', (req, res, next) => {
+//     console.log(req.method, req.url);
+//     next();
+// });
+// app.use('/static',express.static('public'))
+app.use('/static',(req,res,next)=>{
+    console.log(req.method,req.path)
+},express.static("public"))
+
+// app.get('/static/:dir1/:dir2',(req,res)=>{
+//     console.log(req.method,req.params.dir1,req.params.dir2)
+//     res.send("route reached")
+// })
+app.listen(3000)
