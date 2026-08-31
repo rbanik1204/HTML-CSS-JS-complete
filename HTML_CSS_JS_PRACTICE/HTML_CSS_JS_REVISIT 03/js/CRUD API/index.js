c5ff9be0fcf2a4a1c06a474ae6682cf1038f2d46
rbanik1204 <ratulbanik2004@gmail.com>
2026-09-01 00:55:45 +0530

    feat: complete basic CRUD operations

--- HTML_CSS_JS_PRACTICE/HTML_CSS_JS_REVISIT 03/js/CRUD API/index.js
+++ HTML_CSS_JS_PRACTICE/HTML_CSS_JS_REVISIT 03/js/CRUD API/index.js
@@ -9,25 +9,89 @@ app.use((req, res, next) => {
     console.log(req.method + req.path);
     next()
 })
-// app.use(express.urlencoded({ extended: true }))
+app.use(express.urlencoded({ extended: false }))
+app.use(express.json())
 //Routes
-app.get("/users", (req, res) => {
-    // console.log(req.body)
-    const html = `
+app.route("/users")
+    .get((req, res) => {
+        // console.log(req.body)
+        const html = `
     <ul style="list-style-type: none;">
     ${users.map((user) => {
-        return `<li>${user.first_name}&nbsp;${user.last_name}&nbsp;${user.email}</li>`
-    }).join("")}
+            return `<li>${user.first_name}&nbsp;${user.last_name}&nbsp;${user.email}</li>`
+        }).join("")}
     </ul>`
-    res.send(html)
-});
+        res.send(html)
+    })
+    .post((req, res, next) => {
+        const user = { ...req.body, id: users.length + 1 }
+        if (!user.first_name || !user.last_name || !user.email || !user.id || !user.gender || !user.ip_address) {
+            const error = new Error("Unprocessable Entity")
+            error.code = 422
+            return next(error)
+        }
+        users.push(user)
+        fs.writeFile("./data/MOCK_DATA.json", JSON.stringify(users), (err) => {
+            if (err) {
+                err.message = "Internal Server Error"
+                err.code = 500
+                return next(err)
+            }
+            return res.status(201).json({ status: "success" })
+        })
+    });
+app.route("/users/:id")
+    .delete((req, res, next) => {
+        const id = Number(req.params.id);
+        const userIndex = users.findIndex(usr => usr.id === id);
+        if (userIndex === -1) {
+            const error = new Error("Resource not found")
+            error.code = 404
+            return next(error)
+        }
+        users.splice(userIndex, 1);
+        fs.writeFile("./data/MOCK_DATA.json", JSON.stringify(users), (err) => {
+            if (err) {
+                err.message = "Internal Server Error"
+                err.code = 500
+                return next(err)
+            }
+            return res.sendStatus(204);
+        })
+
+    })
+    .patch((req, res, next) => {
+        const id = Number(req.params.id);
+        const userIndex = users.findIndex(usr => usr.id === id);
+        console.log("BODY:", req.body);
+        console.log("UPDATED USER:", users[userIndex]);
+        if (userIndex === -1) {
+            const error = new Error("Resource not found")
+            error.code = 404
+            return next(error)
+        }
+        Object.assign(users[userIndex], req.body)
+        fs.writeFile("./data/MOCK_DATA.json", JSON.stringify(users), (err) => {
+            if (err) {
+                err.message = "Internal Server Error"
+                err.code = 500
+                return next(err);
+            }
+            return res.status(200).json("Successful patching");
+        })
+    });
 
 //Middleware error Handler 404 Global Error Handler
 app.use((req, res, next) => {
-    res.sendStatus(404);
+    const error = new Error("Resource not found");
+    error.code = 404;
+    return next(error);
 })
+app.use((err, req, res, next) => {
+    res.status(err.code || 500).send(err.message || "Internal Server Error");
+});
 //Server Listener 
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, "localhost", () => {
     console.log("server running on ", PORT)
 })
