ddcc09274eadf5ab4cf286c10d712392e2119e79
rbanik1204 <ratulbanik2004@gmail.com>
2026-09-03 17:52:54 +0530

    chore: normalize CRLF and LF line endings

--- HTML_CSS_JS_PRACTICE/HTML_CSS_JS_REVISIT 03/js/REST API/index.js
+++ HTML_CSS_JS_PRACTICE/HTML_CSS_JS_REVISIT 03/js/REST API/index.js
@@ -11,7 +11,7 @@ app.use((req, res, next) => {
     next()
 })
 app.use(express.urlencoded({ extended: false }))
-
+app.use(express.json())
 app.get("/users", (req, res) => {
     const html = `
         <ul> 
