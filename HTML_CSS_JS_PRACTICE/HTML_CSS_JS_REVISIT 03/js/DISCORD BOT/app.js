ba893832033c73f3596fe07f332b2aea296de999
rbanik1204 <ratulbanik2004@gmail.com>
2026-09-14 19:13:47 +0530

    Feat: Discord Bot creation

--- HTML_CSS_JS_PRACTICE/HTML_CSS_JS_REVISIT 03/js/DISCORD BOT/app.js
+++ HTML_CSS_JS_PRACTICE/HTML_CSS_JS_REVISIT 03/js/DISCORD BOT/app.js
@@ -1,15 +1,27 @@
-import {Client,Events,GatewayIntentBits} from 'discord.js'
+import { Client, Events, GatewayIntentBits } from 'discord.js'
 import dotenv from 'dotenv'
 dotenv.config()
-const client = new Client({intents:[GatewayIntentBits.Guilds,
+const client = new Client({
+    intents: [GatewayIntentBits.Guilds,
     GatewayIntentBits.MessageContent,
     GatewayIntentBits.GuildMessages
-]})
-client.on("messageCreate",(message)=>{
-    console.log(message.content);
-    if(message.author.bot) return;
-    message.reply({
-        content:"Hi from Broken Bot!"
-    })
+    ]
+})
+client.on("messageCreate", (message) => {
+    if (message.author.bot) return;
+    if (message.content.startsWith('create')) {
+        console.log(message.content);
+        const url = message.content.split("create ")[1];
+        return message.reply({ content: "Short ID of " + url });
+    }
+    else
+        message.reply({
+            content: "Hi from Broken Bot!"
+        })
 });
-client.login(process.env)
+
+client.on('interactionCreate', (interaction) => {
+    console.log(interaction)
+    interaction.reply("pong")
+})
+client.login(process.env.DISCORD_TOKEN)
--- HTML_CSS_JS_PRACTICE/HTML_CSS_JS_REVISIT 03/js/DISCORD BOT/command.js
+++ HTML_CSS_JS_PRACTICE/HTML_CSS_JS_REVISIT 03/js/DISCORD BOT/command.js
@@ -8,12 +8,12 @@ const commands = [
   },
 ];
 
-const rest = new REST({ version: '10' }).setToken(TOKEN);
+const rest = new REST({ version: '10' }).setToken(process.env.DISCORD_TOKEN);
 
 try {
   console.log('Started refreshing application (/) commands.');
 
-  await rest.put(Routes.applicationCommands(DISCORD_TOKEN), { body: commands });
+  await rest.put(Routes.applicationCommands(process.env.CLIENT_ID), { body: commands });
 
   console.log('Successfully reloaded application (/) commands.');
 } catch (error) {
