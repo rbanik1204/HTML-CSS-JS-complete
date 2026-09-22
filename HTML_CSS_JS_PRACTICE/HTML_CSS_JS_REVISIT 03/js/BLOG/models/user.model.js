af2763679ce0736c793ff4272ca60719f87433e2
rbanik1204 <ratulbanik2004@gmail.com>
2026-09-22 16:32:05 +0530

    Feat:compiled constructor creation User

--- HTML_CSS_JS_PRACTICE/HTML_CSS_JS_REVISIT 03/js/BLOG/models/user.model.js
+++ HTML_CSS_JS_PRACTICE/HTML_CSS_JS_REVISIT 03/js/BLOG/models/user.model.js
@@ -1,10 +1,32 @@
 import {Schema,model} from 'mongoose'
 
 const userSchema = new Schema({
+    fullName:{
+        type:String,
+        required:true
+    },
+    email:{
+        type:String,
+        required:true,
+        unique:true
+    },
     username:{
         type:String,
         required:true,
         unique:true
+    },
+    salt:{
+        type:String,
+        required:true
+    },
+    password:{
+        type:String,
+        required:true
+    },
+    profileImageURL:{
+        type:String,
+        required:true,
+        default:
     },
     role:{
         type:String,
@@ -13,7 +35,7 @@ const userSchema = new Schema({
         },
         required:true
     }
-})
+},{timestamps:true})
 
 const User= model("user",userSchema)// compiled constructor class that provides prgramming interface to run CRUD queries it represents the collection as a whole
 
